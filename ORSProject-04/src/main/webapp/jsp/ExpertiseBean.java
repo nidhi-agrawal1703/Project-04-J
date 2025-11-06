7608753b41be2154bbf0250e5f2627d765481f76
Nidhi Agrawal <nidhi.agrawal1703@yahoo.com>
2025-11-06 17:44:14 +0530

    Add files via upload

---
+++ ORSProject-04/src/main/webapp/jsp/ExpertiseBean.java
@@ -0,0 +1,44 @@
+package in.co.rays.proj4.bean;
+
+public class ExpertiseBean extends BaseBean {
+
+		private long id;
+		private String name;
+		private String description;
+		
+		
+	public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+	public String getKey() {
+		// TODO Auto-generated method stub
+		return id+"";
+	}
+
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+}
